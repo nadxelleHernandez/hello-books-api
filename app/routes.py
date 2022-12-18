efb8f3d3cd3a7c2b6e9ba97c6547c3d6dfa42be5
nadxelle <nadxellehernandez@gmail.com>
2022-12-18 14:49:10 -0800

    Created a GET response handler that gets data from the database. Specifically all the books

--- app/routes.py
+++ app/routes.py
@@ -4,24 +4,25 @@ from flask import Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+@books_bp.route("", methods=["GET"])
+def get_all_books():
+    books_response = []
+    books = Book.query.all()
+    for book in books:
+        books_response.append(book.to_dict())
+    return jsonify(books_response)
+
 @books_bp.route("", methods=["POST"])
-def handle_books():
+def create_book():
     request_body = request.get_json()
     new_book = Book(title=request_body["title"],
                     description=request_body["description"])
 
     db.session.add(new_book)
     db.session.commit()
-
     return make_response(f"Book {new_book.title} successfully created", 201)
-'''
-@books_bp.route("", methods=["GET"])
-def handle_books():
-    books_response = []
-    for book in books:
-        books_response.append(book.to_dict())
-    return jsonify(books_response)
 
+'''
 def validate_book(book_id):
     try:
         book_id = int(book_id)

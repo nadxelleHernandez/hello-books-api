f6103674e6a9d1eda443e786b93dc263971bb3f6
nadxelle <nadxellehernandez@gmail.com>
2022-12-18 14:23:45 -0800

    Created a POST request handler that save a json object to the database

--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,19 @@
-from flask import Blueprint, jsonify, make_response, abort
-
-'''
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "description": self.description,
-        }
-
-
-books = [
-    Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-    Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-] '''
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+@books_bp.route("", methods=["POST"])
+def handle_books():
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"])
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(f"Book {new_book.title} successfully created", 201)
 '''
 @books_bp.route("", methods=["GET"])
 def handle_books():

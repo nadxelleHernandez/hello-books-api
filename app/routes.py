714aa51c1679be39e50e25c658c0748327ac1c60
nadxelle <nadxellehernandez@gmail.com>
2022-12-17 23:14:24 -0800

    refactored some of the handlers

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, make_response, abort
 
 class Book:
     def __init__(self, id, title, description):
         self.id = id
         self.title = title
         self.description = description
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+        }
+
 
 books = [
     Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
@@ -18,20 +26,23 @@ books_bp = Blueprint("books", __name__, url_prefix="/books")
 def handle_books():
     books_response = []
     for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
+        books_response.append(book.to_dict())
     return jsonify(books_response)
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
+    for book in books:
+        if book.id == book_id:
+            return book
+
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def handle_book(book_id):
-    book_id = int(book_id)
-    for book in books:
-        if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }
+    book = validate_book(book_id)
+
+    return jsonify(book.to_dict())

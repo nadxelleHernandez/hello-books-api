9f5432b28f44052e5c303139a90699bf3908d975
nadxelle <nadxellehernandez@gmail.com>
2022-12-20 13:25:44 -0700

    Added delete books functionality

--- app/routes.py
+++ app/routes.py
@@ -54,3 +54,12 @@ def update_book(book_id):
     db.session.commit()
 
     return make_response(f"Book #{book.id} successfully updated")
+
+@books_bp.route("/<book_id>", methods=["DELETE"])
+def delete_book(book_id):
+    book = validate_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully deleted")

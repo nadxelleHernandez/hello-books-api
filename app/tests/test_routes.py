7f3eea859b444b3924191600925e995ecb263071
nadxelle <nadxellehernandez@gmail.com>
2023-01-04 09:19:44 -0800

    changed all validate_book to validate_model

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -186,7 +186,7 @@ def test_delete_book_invalid_id(client, two_saved_books):
     assert response.status_code == 400
     assert response_body == {"message": "Book cat invalid"}
 
-def test_validate_book(two_saved_books):
+def test_validate_model(two_saved_books):
     # Act
     result_book = validate_model(Book,1)
 
@@ -195,14 +195,14 @@ def test_validate_book(two_saved_books):
     assert result_book.title == "Ocean Book"
     assert result_book.description == "watr 4evr"
 
-def test_validate_book_missing_record(two_saved_books):
+def test_validate_model_missing_record(two_saved_books):
     # Act & Assert
     # Calling `validate_book` without being invoked by a route will
     # cause an `HTTPException` when an `abort` statement is reached 
     with pytest.raises(HTTPException):
         result_book = validate_model(Book,"3")
     
-def test_validate_book_invalid_id(two_saved_books):
+def test_validate_model_invalid_id(two_saved_books):
     # Act & Assert
     # Calling `validate_book` without being invoked by a route will
     # cause an `HTTPException` when an `abort` statement is reached 

216e729ed741dc0ed62e12bec0cea83e5c6aa6e4
nadxelle <nadxellehernandez@gmail.com>
2023-01-04 16:34:55 -0800

    added the book and author relationship. Migrated database

--- app/models/author.py
+++ app/models/author.py
@@ -3,6 +3,7 @@ from app import db
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable = False)
+    books = db.relationship("Book", back_populates="author")
 
     def to_dict(self):
         return {

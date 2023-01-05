8e518202741a49cb5a0c5b244ded77ecc491a851
nadxelle <nadxellehernandez@gmail.com>
2023-01-04 17:13:36 -0800

    created endpoint for geting all books by author

--- app/routes/author_routes.py
+++ app/routes/author_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, make_response, request,abort
 from app import db
 from app.models.author import Author
+from app.models.book import Book
+from app.routes.book_routes import validate_model
 
 author_bp = Blueprint("authors", __name__, url_prefix="/authors")
 
@@ -21,3 +23,29 @@ def get_all_authors():
     authors_response = [author.to_dict() for author in authors]
 
     return jsonify(authors_response)
+
+@author_bp.route("/<author_id>/books", methods=["POST"])
+def create_book_by_author(author_id):
+    author = validate_model(Author,author_id)
+ 
+    request_body = request.get_json()
+    new_book = Book(
+        title=request_body["title"],
+        description=request_body["description"],
+        author=author
+    )
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(jsonify(f"Book {new_book.title} by {new_book.author.name} successfully created"), 201)
+
+@author_bp.route("/<author_id>/books", methods=["GET"])
+def get_books_by_author(author_id):
+    author = validate_model(Author,author_id)
+ 
+    books = author.books
+
+    books_response = [book.to_dict() for book in books]
+
+    return jsonify(books_response)

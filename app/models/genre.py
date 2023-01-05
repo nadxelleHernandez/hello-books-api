3e71de4c326562e4d52c294d7458dfc0a4928687
nadxelle <nadxellehernandez@gmail.com>
2023-01-05 12:03:06 -0800

    created the many-to-many model, table and relationships

--- app/models/genre.py
+++ app/models/genre.py
@@ -3,6 +3,7 @@ from app import db
 class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
+    books = db.relationship("Book",secondary="book_genre",back_populates="genres")
 
     def to_dict(self):
         return {
